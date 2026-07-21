422c80e94fcb68685651d547fe83d73bc161f67b
t3i8m <number.38.tj@gmail.com>
2026-07-21 18:18:40 +0200

    Freezed requirements

--- api/config.py
+++ api/config.py
@@ -1,3 +1,3 @@
 LOW_RISK_THRESHOLD = 0.205
 HIGH_RISK_THRESHOLD = 0.830
-DECISION_THRESHOLD = 0.5
+DECISION_THRESHOLD = 0.5 # default thrshold, used only to give a binary prediction, however we rely on the risk levels based on the output probability 
--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-THRESHOLD_ONLINE = 0.5
-THRESHOLD_PHYSICAL = 0.5
+THRESHOLD_ONLINE = 0.5 # default threshold used during the training phase
+THRESHOLD_PHYSICAL = 0.5 # default threshold used during the training phase
 COLS_TO_DROP_ONLINE = ["id",'card_swipe_ratio',"mcc_risk","merchant_risk","card_type","date","merchant_state_risk","location_type", "prev_fraud_count_30days", "mcc", "client_id", "errors", "customer_id", "card_id", "merchant_id", "merchant_city", "use_chip","is_online",  "merchant_state", "zip", "target", "has_bad_cvv"]
 CATEGORIC_COLS_ONLINE = [] # 'mcc_category', "errors"
 NUMERIC_COLS_ONLINE = ['amount', 'hour', 'day_of_week', 'month',  "online_history_ratio", "time_since_last_trx", "trx_count_1h", "trx_amount_1h"]
